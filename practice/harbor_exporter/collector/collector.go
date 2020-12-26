f7c8dabc812e38f64d6b298201c3be6eeccc1660
Desist-Daydream <39847025+Desist-Daydream@users.noreply.github.com>
2020-12-27 01:07:12 +0800

    2020.12.27

--- practice/harbor_exporter/collector/collector.go
+++ practice/harbor_exporter/collector/collector.go
@@ -4,8 +4,8 @@ import (
 	simplejson "github.com/bitly/go-simplejson"
 )
 
-// HarborHealthGatherer 获取 Harbor 各组件健康状态
-func HarborHealthGatherer(h *HarborConnInfo) {
+// HarborHealthCollector 获取 Harbor 各组件健康状态
+func HarborHealthCollector(h *HarborConnInfo) {
 	var healthstatus float64
 	// 使用指定的 endpoint 从 Harbor API 中获取信息
 	body, _ := h.HarborConn("/health")
