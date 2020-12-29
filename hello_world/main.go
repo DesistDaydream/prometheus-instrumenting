d61cb11dd06be4f399a88e21021e189565104927
DesistDaydream <373406000@qq.com>
2020-12-29 11:57:39 +0800

    我再次入门了！而且 hello world 全是自己写的！

--- hello_world/main.go
+++ hello_world/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"math/rand"
 	"net/http"
 	"sync"
@@ -10,78 +9,63 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-// Metrics 用来保存所有 Metrics
-type Metrics struct {
-	MetricsDesc map[string]*prometheus.Desc
-	mutex       sync.Mutex
+// HelloWorldMetrics 用来保存所有 Metrics
+type HelloWorldMetrics struct {
+	HelloWorldDesc *prometheus.Desc
+	mutex          sync.Mutex
 }
 
-// NewMetrics 实例化所有的 Metrics
-func NewMetrics() *Metrics {
-	return &Metrics{
-		MetricsDesc: map[string]*prometheus.Desc{
-			// 定义一个 Metrics 信息，名称、帮助信息、标签 等等
-			"exporter_hello_world": prometheus.NewDesc(
-				"exporter_hello_world",               // Metric 名称
-				"Help Info for exporter hello world", // Metric 的帮助信息
-				[]string{"name"},                     // Metric 的可变标签值的标签
-				nil,                                  // Metric 的不可变标签值的标签
-			),
-			// 还可以定义第二个、第三个.... Metrics
-		},
+// NewMetrics 实例化所有的 Metrics,并为 Mestirs 设定一些基本信息
+func NewMetrics() *HelloWorldMetrics {
+	return &HelloWorldMetrics{
+		HelloWorldDesc: prometheus.NewDesc(
+			"a_hello_world_exporter",              // Metric 名称
+			"Help Info for Hello World Exporter ", // Metric 的帮助信息
+			[]string{"name"}, nil,                 // Metric 的可变标签值的标签 与 不可变标签值的标签
+		),
 	}
 }
 
-// Describe 让 Metrics 实现 Collector 接口。获取所有 Metrics 的描述符
-func (c *Metrics) Describe(ch chan<- *prometheus.Desc) {
-	for _, m := range c.MetricsDesc {
-		ch <- m
-	}
+// Describe 让 HelloWorldMetrics 实现 Collector 接口。获取所有 Metrics 的描述符
+func (ms *HelloWorldMetrics) Describe(ch chan<- *prometheus.Desc) {
+	ch <- ms.HelloWorldDesc
 }
 
-// Collect 让 Metrics 实现 Collector 接口。采集 Metrics 的具体行为
-func (c *Metrics) Collect(ch chan<- prometheus.Metric) {
-	c.mutex.Lock() // 加锁
-	defer c.mutex.Unlock()
+// Collect 让 HelloWorldMetrics 实现 Collector 接口。采集 Metrics 的具体行为
+func (ms *HelloWorldMetrics) Collect(ch chan<- prometheus.Metric) {
+	ms.mutex.Lock() // 加锁
+	defer ms.mutex.Unlock()
 
-	// 获取 Metrics 的值
-	data := c.Set()
-	// 为 Metric 及其 标签 赋予具体的值
-	// 如果有多个 Metircs 则写多个 for 循环即可
-	for name, currentValue := range data {
-		// MustNewConstMetric() 返回一个具有固定值且无法更改的 Metric，需要传递的四个参数分别为：
-		// Metric 的 Desc、Metric 的值类型、当前 Metric 的值、Metric 的标签的值...(可以有多个标签值)
-		ch <- prometheus.MustNewConstMetric(c.MetricsDesc["exporter_hello_world"], prometheus.CounterValue, float64(currentValue), name)
-	}
-}
-
-// Set 为 Metric 设置值。如果由多个 Metrics，则可以设置多个返回值
-func (c *Metrics) Set() (value map[string]int) {
-	value = map[string]int{
-		// desistdaydream 和 nana 是这个 Metric 的 name 标签的值
-		// 使用 rand 生成的随机数，是具有不同标签值的 Metric 的值
-		"desistdaydream": int(rand.Int31n(1000)),
-		"nana":           int(rand.Int31n(1000)),
-	}
-	return
+	// 为 ms.HelloWorldDesc 这个 Metric 设置其属性的值
+	// 该 Metric 值的类型为 Gauge，name 标签值为 haohao 时，Metric 的值为 1000 以内的随机数
+	ch <- prometheus.MustNewConstMetric(ms.HelloWorldDesc, prometheus.GaugeValue, float64(rand.Int31n(1000)), "haohao")
+	// 该 Metric 值的类型为 Gauge，name 标签值为 nana 时，Metric 的值为 100 以内的随机数
+	ch <- prometheus.MustNewConstMetric(ms.HelloWorldDesc, prometheus.GaugeValue, float64(rand.Int31n(100)), "nana")
 }
 
 func main() {
 	// 实例化自己定义的所有 Metrics
 	m := NewMetrics()
-	// 实例化一个默认的注册器
-	registry := prometheus.NewRegistry()
-	// 注册我们自己定义的所有 Metrics
-	registry.MustRegister(m)
+
+	// 两种注册 Metrics 的方式
+	//
+	// 第一种：实例化一个新注册器，用来注册 自定义Metrics
 	// 使用 HandlerFor 将自己定义的已注册的 Metrics 作为参数，以便可以通过 http 获取 metric 信息
-	http.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(m)
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+	//
+	// 第二种：使用自带的默认注册器，用来注册 自定义Metrics
+	// prometheus.MustRegister(m)
+	// http.Handle("/metrics", promhttp.Handler())
+
+	// 让该 exporter 监听在8080 上
+	http.ListenAndServe(":8080", nil)
 }
 
 /*
 Export 暴露结果：
-# HELP exporter_hello_world Help Info for exporter hello world
-# TYPE exporter_hello_world counter
-exporter_hello_world{name="desistdaydream"} 81
-exporter_hello_world{name="nana"} 887
+# HELP a_hello_world_exporter Help Info for Hello World Exporter
+# TYPE a_hello_world_exporter counter
+a_hello_world_exporter{name="DesistDaydream"} 59
 */
